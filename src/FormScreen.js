3ba14b60a039524ad4fe305a30a92bd2fc755a7f
Dileep <dileep335151@gmail.com>
2023-12-04 12:29:38 +0530

    Date display

--- src/FormScreen.js
+++ src/FormScreen.js
@@ -197,6 +197,26 @@ function FormScreen() {
     padding: 0,
   });
 
+  const showData = (dateString) => {
+    if (dateString) {
+      const date = new Date(dateString);
+
+      // Get the year, month, day, hours, minutes, and seconds
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month is zero-based
+      const day = date.getDate().toString().padStart(2, "0");
+      const hours = date.getHours().toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+      const seconds = date.getSeconds().toString().padStart(2, "0");
+      const milliseconds = date.getMilliseconds();
+
+      // Create a readable date format
+      const readableDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+      return readableDate;
+    }
+  };
+
   return (
     <Box p={2}>
       <Typography variant="h4" gutterBottom>
@@ -231,7 +251,7 @@ function FormScreen() {
                 <ListItem key={formData.id}>
                   <ListItemText
                     primary={formData.name}
-                    secondary={Date(formData?.createdAt)}
+                    secondary={showData(formData?.createdAt)}
                   />
                   <ListItemSecondaryAction>
                     <IconButton
